269c046199706254000d5b2381fb912fb3847210
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-26 20:43:01 +0300

    51,54

---
+++ 51/Program.cs
@@ -0,0 +1,23 @@
+﻿// 51. С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+void Morezero(int[] a, int s = 0)
+{
+    for (int i = 0; i < a.Length; i++)
+        if (a[i] > 0) s++;
+    System.Console.WriteLine($"Количество чисел больше 0 - {s}");
+}
+
+void Read(int[]a)
+{
+    for (int i=0;i<a.Length;i++)
+    a[i]=Convert.ToInt32(Console.ReadLine());
+}
+
+int s=0;
+int N=Convert.ToInt32(Console.ReadLine());
+int[]a=new int[N];
+Read(a);
+Morezero(a, s);
+
+
+
---
+++ 54/Program.cs
@@ -0,0 +1,20 @@
+﻿// 54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+void Fib(int N, int[]a)
+{
+    a[0]=0;
+    a[1]=1;
+    for (int i=2;i<a.Length;i++)
+    a[i]=a[i-1]+a[i-2];
+}
+
+void Print(int [] a)
+{
+    for (int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],5}");
+}
+
+int N=Convert.ToInt32(Console.ReadLine());
+int[]a=new int[N];
+Fib(N,a);
+Print(a);
